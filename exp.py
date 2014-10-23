3bf78f9e3ff235946377508c9c28b0255c3f2c50
AK <andrei.avk@gmail.com>
2014-10-22 22:09:37 -0400

    updated exp() to handle `n` values which are not powers of 2

--- exp.py
+++ exp.py
@@ -2,31 +2,41 @@
 
 # Exercise 1.16
 def exp(b, n):
-    """Calc b**n using only squaring operation."""
-    a   = b
-    a_n = 1
-    while True:
-        a **= 2
-        a_n *= 2
-        if (n-a_n)==0:
-            return a
-        # if y>=8: break
+    """ Calc b**n using only squaring operation.
 
-        print("a", a)
-        print("a_n", a_n)
-        print("a * b ** (n-a_n)", a * b**(n-a_n))
-        print()
+        `n` needs to be an integer because otherwise we would need to use raise to `m` operation where
+        `m` != 2, or an equivalent operation, which is not allowed in this task.
+    """
+    if n==0: return 1
+    a   = b
+    a_n = next_n = 1
+
+    while True:
+        next_n *= 2
+
+        # if cannot use squaring anymore, go the rest of way by repeated multiplication by `b`
+        if (n-next_n) < 0:
+            for _ in range(n-a_n):
+               a *= b
+            return a
+        else:
+            a_n = next_n
+
+        a **= 2
+        if (n-a_n) == 0:
+            return a
 
 # print('answer!  ', exp(5, 32), '\n')
-print('answer!  ', exp(5, 64), '\n')
+b, n = 2, 69
+print('answer:', exp(b, n), '\n')
 
-def n(expr):
+def f(expr):
     print(expr, '==', eval(expr))
-n("5**64")
+f("%s**%s" % (b, n))
 
 if 0:
-    n("2**0 * 2**64")
-    n("2**1 * 2**63")
-    n("2**2 * 2**62")
-    n("2**4 * 2**60")
-    n("2**8 * 2**56")
+    f("2**0 * 2**64")
+    f("2**1 * 2**63")
+    f("2**2 * 2**62")
+    f("2**4 * 2**60")
+    f("2**8 * 2**56")
